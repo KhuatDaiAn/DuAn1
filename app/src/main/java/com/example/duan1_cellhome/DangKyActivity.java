452220c5f841e4e1716a925530f14dbd410aa7f2
KhuatDaiAn <daiankhuat199@gmail.com>
2021-11-05 22:23:00 +0700

    layout đăng ký đăng nhập

---
+++ app/src/main/java/com/example/duan1_cellhome/DangKyActivity.java
@@ -0,0 +1,14 @@
+package com.example.duan1_cellhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DangKyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ky);
+    }
+}
--- app/src/main/java/com/example/duan1_cellhome/DangNhapActivity.java
+++ app/src/main/java/com/example/duan1_cellhome/DangNhapActivity.java
@@ -2,13 +2,26 @@ package com.example.duan1_cellhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DangNhapActivity extends AppCompatActivity {
-
+    private TextView txtDangKy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
+        txtDangKy = findViewById(R.id.txtDangKy);
+
+
+        txtDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DangNhapActivity.this, DangKyActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
