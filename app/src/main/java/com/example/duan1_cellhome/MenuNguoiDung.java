30f1eeb6618edbd9862062af9822407b1a01a335
hohoangphuc1907 <84660182+hohoangphuc1907@users.noreply.github.com>
2021-11-05 11:42:06 +0700

    update/menu

--- app/src/main/java/com/example/duan1_cellhome/MenuNguoiDung.java
+++ app/src/main/java/com/example/duan1_cellhome/MenuNguoiDung.java
@@ -1,36 +1,36 @@
 package com.example.duan1_cellhome;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.duan1_cellhome.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
-//test
+import com.example.duan1_cellhome.databinding.ActivityMenunguoidungBinding;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MenuNguoiDung extends AppCompatActivity {
+
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
+    private ActivityMenunguoidungBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMenunguoidungBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
 
-        DrawerLayout drawer = binding.drawerLayout;
+        DrawerLayout drawer = binding.drawerLayoutnguoidung;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -56,4 +56,7 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/duan1_cellhome/NhaFragment.java
@@ -0,0 +1,6 @@
+package com.example.duan1_cellhome;
+
+import androidx.fragment.app.Fragment;
+
+public class NhaFragment extends Fragment {
+}
