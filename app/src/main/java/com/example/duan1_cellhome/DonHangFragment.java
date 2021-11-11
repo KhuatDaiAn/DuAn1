013ba5912df31cbb5fd5899a5552f24059a55624
hohoangphuc1907 <84660182+hohoangphuc1907@users.noreply.github.com>
2021-11-11 22:09:51 +0700

    update

--- app/src/main/java/com/example/duan1_cellhome/DonHangFragment.java
+++ app/src/main/java/com/example/duan1_cellhome/DonHangFragment.java
@@ -1,5 +1,12 @@
 package com.example.duan1_cellhome;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.duan1_cellhome.Model.DonHang;
@@ -9,4 +16,11 @@ import java.util.List;
 
 public class DonHangFragment extends Fragment {
     List<DonHang> donHangList = new ArrayList<>();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
 }
